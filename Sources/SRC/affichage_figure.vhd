909d9849b627e97764bb1c470ef2b3396259fc11
Clement <56961627+Verboom30@users.noreply.github.com>
2021-03-10 17:17:26 +0100

    Update affichage_figure.vhd

--- Sources/SRC/affichage_figure.vhd
+++ Sources/SRC/affichage_figure.vhd
@@ -47,12 +47,15 @@ entity affichage_figure is
 end affichage_figure;
 
 architecture Behavioral of affichage_figure is
+ TYPE STATE_TYPE IS (WAITING_ON,WAITING_OFF, RUNNING,CHECK);
+ SIGNAL cstate : STATE_TYPE;
 begin
     process(clk,reset)
     --variable Nb_pixel_HG: integer range 0 to 15999;
     --variable Nb_pixel_BD: integer range 0 to 15999;
     variable Nb_HGL:integer range 0 to 100;
     variable Nb_HGC:integer range 0 to 160;
+    variable Nb_HGC_cpy:integer range 0 to 160;
     variable Nb_BDL:integer range 0 to 100;
     variable Nb_BDC:integer range 0 to 160;
     variable cpt_c: integer range 0 to 8;
@@ -61,6 +64,7 @@ begin
     variable flag: boolean :=false;
     begin
         if(reset = '1') then
+            cstate   <= WAITING_ON;
             cpt_l := 0;
             cpt_c := 0;
             cpt := 0;
@@ -68,48 +72,68 @@ begin
             --Nb_pixel_BD:=0;
             Nb_HGL:=0;
             Nb_HGC:=0;
+            Nb_HGC_cpy:=0;
             Nb_BDL:=0;
             Nb_BDC:=0;
             out_R_W <='0';
+            flag:=false;
             out_adr<="00000000000000";
         elsif (clk'event and clk ='1') then
             if (ce = '1') then
-                if( in_affichage_en = '1') then
-                         Nb_HGL:=to_integer(unsigned(in_HG(7 downto 0)));
-                         Nb_HGC:=to_integer(unsigned(in_HG(15 downto 8)));
-                         Nb_BDL:=to_integer(unsigned(in_BD(7 downto 0)));
-                         Nb_BDC:=to_integer(unsigned(in_BD(15 downto 8)));
-                         --Nb_pixel_HG:=160*to_integer(unsigned(in_HG(7 downto 0)))+to_integer(unsigned(in_HG(15 downto 8)));
-                         --Nb_pixel_BD:=160*to_integer(unsigned(in_BD(7 downto 0)))+to_integer(unsigned(in_BD(15 downto 8)));
-                         cpt_l := 0;
-                         cpt_c := 0;
-                         cpt:=0;
-                         out_R_W<='1';
-                         flag:=false;
-                         
-                 elsif(Nb_HGC>Nb_BDC or Nb_HGL>Nb_BDL or Nb_BDC>160 or Nb_HGC>160 or Nb_BDL>100 or Nb_HGL>100 or Nb_BDC<0 or Nb_HGC<0 or Nb_BDL<0 or Nb_HGL<0) then
-                    --ERREUR
-                        flag:=false;
-                        out_adr <="11111111111111";
-                 else
-                    if(flag=true)then
-                         if(cpt_c /= (Nb_BDC-Nb_HGC) or cpt_l /= (Nb_BDL-Nb_HGL)) then
-                             cpt:=cpt+1;
-                             if(cpt_c<(Nb_BDC-Nb_HGC)) then
-                                    cpt_c := cpt_c + 1;
-                             else
-                                    cpt_c:=0;
-                                    cpt_l:=cpt_l+1;
-                             end if;
-                         else
-                               out_R_W<='0';
-                         end if;
-                    end if;
-                        flag:=true;
-                        out_adr <= std_logic_vector(to_unsigned((160*to_integer(unsigned(in_HG(7 downto 0)))+to_integer(unsigned(in_HG(15 downto 8))))+cpt_c+(160*cpt_l), 14));
-                        out_adr_firgure<= std_logic_vector(to_unsigned(cpt,14));
-                 end if;    
+                        CASE cstate IS
+                    
+                            WHEN WAITING_ON =>
+                                
+                                if( in_affichage_en = '1') then
+                                    cstate <= WAITING_OFF;
+                                else
+                                    cstate <= WAITING_ON;
+                                end if;
+                              
+                             
+                            WHEN WAITING_OFF =>
+                                 
+                                if( in_affichage_en = '0') then
+                                
+                                        Nb_HGL:=to_integer(unsigned(in_HG(7 downto 0)));
+                                        Nb_HGC:=to_integer(unsigned(in_HG(15 downto 8)));
+                                        Nb_BDL:=to_integer(unsigned(in_BD(7 downto 0)));
+                                        Nb_BDC:=to_integer(unsigned(in_BD(15 downto 8)));
+                                     cstate <= CHECK;
+                                else
+                                     cstate <= WAITING_OFF;
+                                end if;
+                             WHEN CHECK=>
+                                    if(Nb_HGC>Nb_BDC or Nb_HGL>Nb_BDL or Nb_BDC>160 or Nb_HGC>160 or Nb_BDL>100 or Nb_HGL>100 or Nb_BDC<0 or Nb_HGC<0 or Nb_BDL<0 or Nb_HGL<0)then
+                                        cstate <=WAITING_ON;
+                                    else
+                                        cpt_l := 0;
+                                        cpt_c := 0;
+                                        cpt:=0;
+                                        Nb_HGC_cpy:=Nb_HGC;
+                                        cstate <=RUNNING;
+                                        out_R_W<='1';
+                                    end if;
+                             WHEN RUNNING =>
+                        
+                                     if(cpt_c /= (Nb_BDC-Nb_HGC) or cpt_l /= (Nb_BDL-Nb_HGL)) then
+                                         cpt:=cpt+1;
+                                         if(cpt_c<(Nb_BDC-Nb_HGC)) then
+                                                cpt_c := cpt_c + 1;
+                                         else
+                                                cpt_c:=0;
+                                                cpt_l:=cpt_l+1;
+                                         end if;
+                                           cstate <= RUNNING;
+                                     else
+                                           out_R_W<='0';
+                                           cstate <= WAITING_ON;
+                                     end if;
+                           END CASE;
+                           out_adr <= std_logic_vector(to_unsigned( Nb_HGC_cpy+cpt_c+(160*cpt_l), 14));
+                           out_adr_firgure<= std_logic_vector(to_unsigned(cpt,14)); 
             end if;
         end if;
     end process;
 end Behavioral;
+
