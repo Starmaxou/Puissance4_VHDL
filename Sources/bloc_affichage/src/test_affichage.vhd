b436dfe7a29b16f30bfd8f4cec478a941eba5489
Clement <56961627+Verboom30@users.noreply.github.com>
2021-03-12 00:16:56 +0100

    bloc affichage + test

---
+++ Sources/bloc_affichage/src/test_affichage.vhd
@@ -0,0 +1,86 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 09.03.2021 15:06:07
+-- Design Name: 
+-- Module Name: test_affichage - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity test_affichage is
+        port( 
+            clk: in std_logic;
+            Bp1: in std_logic;
+            Bp2: in std_logic;
+            Bp3: in std_logic;
+            Bp4: in std_logic;
+            Bp5: in std_logic;
+            out_HG: out std_logic_vector(15 downto 0);
+            out_BD: out std_logic_vector(15 downto 0);
+            out_num_figure: out std_logic_vector( 2 downto 0);
+            out_en_affichage_test: out std_logic);
+end test_affichage;
+
+architecture Behavioral of test_affichage is
+signal flag: std_logic :='0';
+  begin
+    process (clk)
+        begin
+            if(clk = '1' and clk'event) then
+            
+                if(Bp1 ='1' and Bp2 ='0'and Bp3 ='0'and Bp4 ='0' and Bp5 ='0')then
+                      out_num_figure<="001";
+                      out_HG<=x"2211";
+                      out_BD<=x"2B1A";
+                elsif(Bp1 ='0' and Bp2 ='1'and Bp3 ='0'and Bp4 ='0' and Bp5 ='0')then
+                      out_num_figure<="010";
+                      out_HG<=  x"3011";
+                      out_BD<= x"391A";
+                elsif(Bp1 ='0' and Bp2 ='0'and Bp3 ='1'and Bp4 ='0' and Bp5 ='0')then
+                      out_num_figure<="011";
+                      out_HG<= x"3E11";
+                      out_BD<= x"471A";
+                elsif(Bp1 ='0' and Bp2 ='0'and Bp3 ='0'and Bp4 ='1' and Bp5 ='0')then
+                      out_num_figure<="100";
+                      out_HG<=x"4C11";                
+                      out_BD<=x"551A";
+                elsif(Bp1 ='0' and Bp2 ='0'and Bp3 ='0'and Bp4 ='0' and Bp5 ='1')then
+                     out_num_figure<="000";
+                     out_HG<=x"0000";
+                     out_BD<=x"9F63";
+                end if;
+                
+            
+            end if;
+    end process;
+  
+   out_en_affichage_test<='0' when (Bp1='0' and Bp2='0' and Bp3='0' and Bp4='0' and Bp5='0') else
+                      '1';
+                              
+                        
+end Behavioral;
---
+++ Sources/bloc_affichage/src/top_vga.vhd
@@ -0,0 +1,142 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02.03.2021 14:06:28
+-- Design Name: 
+-- Module Name: top_vga - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity top_affichage is
+    port( reset : in std_logic ;
+          clk100M : in std_logic;
+          in_HG: in std_logic_vector(15 downto 0);
+          in_BD: in std_logic_vector(15 downto 0);
+          in_affichage_en: in std_logic;
+          num_figure: in std_logic_vector( 2 downto 0);
+          VGA_R: out std_logic_vector(3 downto 0);
+          VGA_B: out std_logic_vector(3 downto 0);
+          VGA_G: out std_logic_vector(3 downto 0);
+          VGA_HS: out std_logic;
+          VGA_VS: out std_logic
+          );
+end top_affichage;
+
+architecture Behavioral of top_affichage is
+    signal ADDR: std_logic_vector(13 downto 0);
+    signal ADDR_reg: std_logic_vector(13 downto 0);
+    signal data_RGB: std_logic_vector(2 downto 0);
+    signal dataout: std_logic_vector(2 downto 0);
+    signal signot_Reset: std_logic;
+    signal R_W: std_logic;
+    signal ADDR_affichage_W: std_logic_vector(13 downto 0);
+    signal ADDR_affichage_R: std_logic_vector(13 downto 0);
+    signal figure_RGB: std_logic_vector(2 downto 0);
+    signal NOT_R_W: std_logic;
+    signal ADDR_fig: std_logic_vector(13 downto 0);
+begin
+    
+    compteur : entity  work.compteur
+      port map (
+                CE =>'1',
+                H=>clk100M,
+                RST=>signot_reset,
+                en_cpt=>NOT_R_W,
+                init_cpt=>'0',
+                dataout=>ADDR_affichage_R
+                );  
+                
+        mux : entity  work.mux
+           port map (
+                sel_mux =>R_W,
+                in_data0 =>ADDR_affichage_R,
+                in_data1 => ADDR_affichage_W,
+                out_data =>ADDR
+                );   
+                
+     reg : entity work.registre  
+        port map(
+                    in_data => ADDR_affichage_R,
+                    ce =>'1',
+                    clk =>clk100M,
+                    reset=>signot_reset,
+                    out_data=> ADDR_reg);
+                    
+    
+                    
+    
+     men_affichage : entity work.memoire
+        port map(
+                en_men=>'1',
+                R_W =>R_W,
+                ce=>'1',
+                clk=>clk100M,
+                in_adr=>ADDR,
+                in_data=>figure_RGB,
+                out_data=>data_RGB);
+                
+      men_figure : entity work.mem_figure
+        port map(
+           clk =>clk100M,
+           ce=>'1',
+           en_mem=>'1',
+           in_addr_figure => ADDR_fig,
+           num_figure=> num_figure,
+           out_data =>figure_RGB);
+           
+           
+      affichage_figure : entity work.affichage_figure
+        port map(
+            ce=>'1',
+            clk =>clk100M,
+            reset =>signot_reset,
+            in_HG =>in_HG,
+            in_BD => in_BD,
+            in_affichage_en=> in_affichage_en,
+            out_adr =>ADDR_affichage_W,
+            out_adr_figure => ADDR_fig,
+            out_R_W=>R_W);
+  
+     VGA: entity  work.VGA_bitmap_160x100
+      port map (
+       clk          =>clk100M,
+       reset        =>signot_reset,
+       VGA_hs       => VGA_HS,
+       VGA_vs       =>VGA_VS,
+       VGA_red      =>VGA_R,
+       VGA_green    =>VGA_G,
+       VGA_blue     =>VGA_B,
+
+       ADDR         =>ADDR_reg,
+       data_in      =>data_RGB,
+       data_write   =>NOT_R_W,
+       data_out => dataout
+                );
+        signot_reset<=not(reset);
+        NOT_R_W<=not(R_W);
+end Behavioral;
+
