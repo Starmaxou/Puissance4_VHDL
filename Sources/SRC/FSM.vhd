ab1ece4641e28a1ac8f7deb6c2fda825a2ee8d79
ALBERTY Maxime <Maxime.Alberty@bordeaux-inp.fr>
2021-03-17 17:20:54 +0100

    Update FSM.vhd Ajout des etats correspondant à FSM.drawio Debut d'ajout des transitions des etats

--- Sources/SRC/FSM.vhd
+++ Sources/SRC/FSM.vhd
@@ -38,23 +38,32 @@ entity FSM is
            btnL                 : in STD_LOGIC;
            btnC                 : in STD_LOGIC;
            btnR                 : in STD_LOGIC;
-           A_read_type_piece    : in STD_LOGIC;
-           B_affichage_ready    : in STD_LOGIC;
-           C_state_victoire     : in STD_LOGIC;
+           A_read_type_piece    : in STD_LOGIC_VECTOR(2  downto 0);
+           B_state_victoire     : in STD_LOGIC_VECTOR(1 downto 0);
            
-           D_ligne_grille       : out STD_LOGIC;
-           E_colonne_grille     : out STD_LOGIC;
-           F_write_type_piece   : out STD_LOGIC
+           C_ligne_grille       : out STD_LOGIC_VECTOR(2 downto 0);
+           D_colonne_grille     : out STD_LOGIC_VECTOR(2 downto 0);
+           E_write_type_piece   : out STD_LOGIC_VECTOR(2 downto 0);
+           F_RW_plateau         : out STD_LOGIC;
+           G_en_verif           : out STD_LOGIC;
+           H_init_verif         : out STD_LOGIC;
+           I_sel_LC             : out STD_LOGIC
            );
 end FSM;
 
 architecture Behavioral of FSM is
 
-    type Etat is ( Etat_init, Etat_Rouge, Etat_Jaune, Etat_Test_Jeu, Etat_Affichage, Etat_Test_Victoire, Etat_Affichage_Victoire, Etat_Victoire);
+    type Etat is ( Etat_init, Etat_Init_grille, Etat_Affichage_jeu, Etat_Effacer_pos, Etat_Incrementer, Etat_Decrementer, Etat_Ecriture_pos, Etat_Check_mouv, Etat_Ecriture_mouv, Etat_Check_victoire, Etat_Victoire);
 	signal pr_state , nx_state : Etat := Etat_init;
 	
+	type Joueur is (Joueur_Rouge, Joueur_Jaune);
+	signal pr_player : Joueur := Joueur_Rouge;
+	
+	signal btn : std_logic := '0';
 begin
-
+------ 
+-- Mise à jour synchrone des etats
+------
     maj_etat : process ( H , RST )
     begin
         if (RST ='1') then
@@ -66,48 +75,94 @@ begin
         end if;
     end process maj_etat;
     
-    cal_nx_state : process (pr_state)
+    maj_joueur : process ( H , RST )
+        begin
+            if (RST ='1') then
+                pr_player <= Joueur_Rouge ;
+            elsif ( H' event and H ='1') then
+                if(CE = '1') then
+                    pr_player <= nx_player;
+                end if;
+            end if;
+        end process maj_etat;
+
+------
+-- Calcul du nouvel etat en fonction des entrées
+------    
+    cal_nx_state : process (pr_state, btnC, btnL, btnR)
         begin
             case pr_state is
                 when Etat_Init =>
-                    nx_state <= Etat_Rouge;
-                when Etat_Rouge =>
+                    nx_state <= Etat_Affichage_jeu;
                     
-                when Etat_Jaune =>
+                --when Etat_Init_grille =>
                     
-                when Etat_Test_Jeu =>
-                    
-                when Etat_Affichage =>
-                    
-                when Etat_Test_Victoire =>
-                    
-                when Etat_Affichage_Victoire =>
-                    
-                when Etat_Victoire =>
+                when Etat_Affichage_jeu =>
+                    if( btnC = '1' ) then
+                        nx_state <= Etat_check_mouv;
+                    elsif( btnL = '1' ) then
+                        btn <= '0';
+                        nx_state <= Etat_Effacer_pos;
+                    elsif( btnR = '1' ) then
+                        btn <= '1';
+                        nx_state <= Etat_Effacer_pos;
+                    end if;
+                
+                when Etat_Effacer_pos =>
+                    -- Ajouter vérification d'écriture de la case blanche
+                    if( btn = '1' ) then
+                        nx_state <= Etat_Incrementer;
+                    else
+                        nx_statue <= Etat_Decrementer;
+                
+                --when Etat_Incrementer =>
+                
+                --when Etat_Decrementer =>
+                
+                --when Etat_Ecriture_pos =>
+                
+                --when Etat_Check_mouv =>
+                
+                --when Etat_Ecriture_mouv =>
+                
+                --when Etat_Check_victoire =>
+                                
+                --when Etat_Victoire =>
                     
                 when others =>
                     nx_state <= Etat_init;
             end case;
         end process cal_nx_state;
-    
-    cal_output : process( pr_state, code_op, carry )
+
+------
+-- Mise à jours des sorties en fonction de l'état courant
+------    
+    cal_output : process( pr_state )
         begin
             case pr_state is
                 when Etat_Init =>
                     
-                when Etat_Rouge =>
-                    
-                when Etat_Jaune =>
-                    
-                when Etat_Test_Jeu =>
-                    
-                when Etat_Affichage =>
-                    
-                when Etat_Test_Victoire =>
-                    
-                when Etat_Affichage_Victoire =>
-                    
-                when Etat_Victoire =>
+                --when Etat_Init_grille =>
+                
+                when Etat_Affichage_jeu =>
+                
+                --when Etat_Effacer_pos =>
+                
+                --when Etat_Incrementer =>
+                
+                --when Etat_Decrementer =>
+                               
+                --when Etat_Ecriture_pos =>
+                
+                --when Etat_Check_mouv =>
+                
+                --when Etat_Ecriture_mouv =>
+                
+                --when Etat_Check_victoire =>
+                
+                --when Etat_Victoire =>
+                
+                when others =>
                                                                                       
             end case;
         end process cal_output;
